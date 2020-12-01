16923eb6946efb3e7252826efdda53225c8a3172
Jason Cobb <jason_m_cobb@carmax.com>
2020-12-01 12:25:16 -0500

    Day 1 and first commit

---
+++ AdventOfCode2020/AdventOfCodeMenu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AdventOfCode2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Which day would you like to run(1-31)?");
+            var inputChoice = Console.ReadLine();
+            Day1 dayOneObject = new Day1();
+            switch (inputChoice)
+            {
+                case "1":
+                    int dayOne = dayOneObject.DayOne();
+                    Console.WriteLine($"Output for Day One is {dayOne}");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
