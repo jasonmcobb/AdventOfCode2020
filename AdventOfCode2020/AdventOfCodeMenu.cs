57845ecbfeb8b4ae2c66dde2a2ed0e99c67540f1
Jason Cobb <jason_m_cobb@carmax.com>
2020-12-02 18:53:40 -0500

    Day 2 solution

--- AdventOfCode2020/AdventOfCodeMenu.cs
+++ AdventOfCode2020/AdventOfCodeMenu.cs
@@ -6,15 +6,24 @@ namespace AdventOfCode2020
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Which day would you like to run(1)?");
-            var inputChoice = Console.ReadLine();
+            // Day objects:
             Day1 dayOneObject = new Day1();
+            Day2 dayTwoObject = new Day2("/Users/jasoncobb/Projects/AdventOfCode2020/AdventOfCode2020/Inputs/Day2.txt");
+
+            Console.WriteLine("Which day would you like to run(1-2)?");
+            var inputChoice = Console.ReadLine();
             switch (inputChoice)
             {
                 case "1":
                     int[] dayOne = dayOneObject.DayOne();
                     Console.WriteLine($"Output for Day One Part One is {dayOne[0]}");
                     Console.WriteLine($"Output for Day One Part Two is {dayOne[1]}");
+                    break;
+                case "2":
+                    var dayTwoPartOneResponse = dayTwoObject.dayTwoPartOne();
+                    var dayTwoPartTwoResponse = dayTwoObject.dayTwoPartTwo();
+                    Console.WriteLine($"Output for Day Two Part One is {dayTwoPartOneResponse}");
+                    Console.WriteLine($"Output for Day Two Part Two is {dayTwoPartTwoResponse}");
                     break;
                 default:
                     Console.WriteLine("You either entered an invalid number or a day that hasn't happened yet.");
--- AdventOfCode2020/Day2.cs
+++ AdventOfCode2020/Day2.cs
@@ -1,10 +1,57 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+
 namespace AdventOfCode2020
 {
     public class Day2
     {
-        public Day2()
+        public string[] fileContents;
+        // public int counter = 0;
+
+        public Day2(string filePath)
         {
+            fileContents = File.ReadAllLines(filePath);
+        }
+
+        public int dayTwoPartOne()
+        {
+            char[] trimChar = { ':' };
+            int counter = 0;
+            foreach (string Line in fileContents)
+            {
+                var TempHolder = Line.Split(" ");
+                Int32.TryParse(TempHolder[0].Split("-")[0], out int lowerBound);
+                Int32.TryParse(TempHolder[0].Split("-")[1], out int upperBound);
+                char[] charToCheck = TempHolder[1].Trim(trimChar).ToCharArray();
+                int charCounter = TempHolder[2].ToCharArray().Count(c => c == charToCheck[0]);
+                if ((lowerBound <= charCounter) && (charCounter <= upperBound))
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        public int dayTwoPartTwo()
+        {
+            int counter = 0;
+            char[] trimChar = { ':' };
+            foreach (string Line in fileContents)
+            {
+                var TempHolder = Line.Split(" ");
+                Int32.TryParse(TempHolder[0].Split("-")[0], out int lowerBound);
+                Int32.TryParse(TempHolder[0].Split("-")[1], out int upperBound);
+                string charToCheck = TempHolder[1].Trim(trimChar);
+                int lowerBoundCheck = TempHolder[2].IndexOf(charToCheck, (lowerBound - 1));
+                int upperBoundCheck = TempHolder[2].IndexOf(charToCheck, (upperBound - 1));
+                // This is a ridiculous if statement, but it works.
+                if (((lowerBound - 1 == lowerBoundCheck) && (upperBoundCheck != upperBound - 1)) || ((lowerBoundCheck != lowerBound - 1) && (upperBound - 1 == upperBoundCheck)))
+                {
+                    counter++;
+                }
+            }
+            return counter;
         }
     }
 }
